9ca0ed5f79a88131668076734b06cd86a293589e
Kauan-Almeida <kauanalmeida2233@gmail.com>
2023-02-01 13:28:24 -0300

    Recebendo funções como parâmetro

---
+++ src/application/Program.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Product;
+import model.services.ProductService;
+
+public class Program {
+	
+	/**Fazer um programa que, a partir de uma lista de produtos, calcule a
+	soma dos preços somente dos produtos cujo nome começa com "T".**/
+	
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.00));
+		list.add(new Product("HD Case", 80.90));
+		
+		ProductService ps = new ProductService();
+		
+		double sum = ps.filteredSum(list, p -> p.getName().charAt(0) == 'T');
+		double som = ps.filteredSum(list, p -> p.getPrice() < 100.00);
+		
+		System.out.println("Sum = " + String.format("%.2f", sum));
+		System.out.println("Som = " + String.format("%.2f", som));
+	}
+}
